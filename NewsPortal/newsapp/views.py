e5872a5d1705afeaf641f24149badccff66fd081
xwalkodawx <xwalkodawx@yandex.ru>
2023-04-10 21:57:17 +0300

    d 3 6

--- NewsPortal/newsapp/views.py
+++ NewsPortal/newsapp/views.py
@@ -12,7 +12,6 @@ class PostList(ListView):
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		context['time_now'] = datetime.utcnow()
-		context['next_sale'] = None
 		return context
 
 
